f589222450916c9cfa5f9a46a9e241964dce00cb
jonathanforhan <jonathan.forhan@gmail.com>
2023-05-18 00:53:18 -0400

    fixed git

--- artix-script.sh
+++ artix-script.sh
@@ -2,17 +2,17 @@
 
 sudo pacman -Syyu
 source "$HOME/.bashrc"
+sudo pacman -S curl git gpg pass
 
 ## Setup Git
-sudo pacman -S git gpg pass
 gpg --gen-key
-gpg --list-keys | awk '/^      /{print }' | xargs pass init 
+gpg --list-keys | awk '/^      /{print $1}' | xargs pass init
 git config --global credentialStore gpg
+curl -L https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.1.2/gcm-linux_amd64.2.1.2.tar.gz | tar zx -C "/usr/local/bin/"
+git-credential-manager configure
 
 ## Setup AUR
 
 ## Setup GUI
-
-# sudo pacman -S xorg-server xorg-xinit xorg-xsetroot
 
 ## Setup Neovim
