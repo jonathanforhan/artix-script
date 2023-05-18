5223abba51c36f81491a46bc4fb9904504f07619
jonathanforhan <jonathan.forhan@gmail.com>
2023-05-18 06:49:11 +0000

    added .config

---
+++ .bash_history
@@ -0,0 +1,44 @@
+xit
+exit
+ll
+v tmux.conf 
+rm -rf plugins/*
+tmux source tmux.conf 
+exit
+tmux source tmux.conf 
+v tmux.conf 
+ll
+cd plugins/
+l
+cd tmux-essentials/
+l
+cd data/
+l
+v nvim_buffer 
+v
+d
+cd
+cd .config/nvim/lua/user/
+v init.lua 
+vim init.lua 
+exit
+xrdb --query
+sudo pacman -S xrdb
+pacman -Ss xrdb
+sudo pacman -S xorg-xrdb
+xrdb --query
+xrdb -query
+cd
+l
+ll
+xrdb merge
+xrdb --help
+xrdb -merge
+tconf
+cd .config/tmux/
+v tmux.conf 
+vim tmux.conf 
+v tmux.conf 
+exit
+l
+ll
--- artix-script.sh
+++ artix-script.sh
@@ -2,20 +2,33 @@
 
 sudo pacman -Syyu
 source "$HOME/.bashrc"
-sudo pacman -S --needed curl git vim neovim base-devel pass 
+sudo pacman -S --needed curl git neovim tmux base-devel pass figlet
 
 ## Setup Git
+figlet -ckt 'Git Setup'
+echo "Setting up git-credential-manager..."
 gpg --gen-key
 gpg --list-keys | awk '/^      /{print $1}' | xargs pass init "$1"
 git config --global credential.credentialStore gpg
 curl -L https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.1.2/gcm-linux_amd64.2.1.2.tar.gz | sudo tar zx -C "/usr/local/bin/"
 git-credential-manager configure
+echo "Linking Github account info"
+read -p "Github username: " git_name
+git config --global user.name "$git_name"
+read -p "Github email: " git_email
+git config --global user.email "$git_email"
+git config --global credential.guiPrompt false
 
 ## Setup AUR
+figlet -ckt 'AUR Setup'
 git clone https://aur.archlinux.org/yay.git
 sudo chown -R "$(whoami):users" yay && cd "$HOME/yay" && makepkg -si
 cd "$HOME" && rm -rf "$HOME/yay"
 
+## Setup Configs
+tmux source "$HOME/.config/tmux/tmux.conf"
+
+## Setup KVM
+
 ## Setup GUI
 
-## Setup Neovim
