20479c4253c57a6b7aa361ff3c53033341d4a0d7
jonathan forhan <jonathan.forhan@gmail.com>
2023-05-18 02:34:52 +0000

    git

--- artix-script.sh
+++ artix-script.sh
@@ -6,7 +6,8 @@ source "$HOME/.bashrc"
 ## Setup Git
 sudo pacman -S git gpg pass
 gpg --gen-key
-pass init
+gpg --list-keys | awk '/^      /{print }' | xargs pass init 
+git config --global credentialStore gpg
 
 ## Setup AUR
 
